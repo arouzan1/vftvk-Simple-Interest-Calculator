03e5fd85a4c32be7e6de545ae2250621378006fe
arouzan1 <96306015+arouzan1@users.noreply.github.com>
2021-12-17 12:20:33 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,10 @@
 function compute()
 {
     p = document.getElementById("principal").value;
-    
+    x = document.getElementById("rateRange").value;
+    y = document.getElementById("years").value;
+    z = p*x*y
+    yr = getYear() - 100 + 2000
+    print("If you deposit "+p+", \n at an interest rate of "+x+"%.\n You will receive an amount of "+z+", \n in the year " + (yr + y)) 
 }
         
