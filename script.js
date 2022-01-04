cfb434c4983bb4d00d85d55a4797893ceed9a62e
arouzan1 <96306015+arouzan1@users.noreply.github.com>
2022-01-04 15:37:19 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,11 +1,29 @@
 function compute()
 {
-    p = document.getElementById("principal").value;
-    x = document.getElementById("rateRange").value;
-    y = document.getElementById("years").value;
-    z = p*x*y
-    yr = getYear() + 1900
-    result = document.getElementById("result");
-    result.innerHTML = "If you deposit " + p + ", \n at an interest rate of " + x + "%.\n You will receive an amount of " + z + ", \n in the year " + (yr + y); 
+    var t = document.getElementById("principal").value;
+    var p = Number(t)
+    if (p<=0) {
+    	alert("Please enter positive Principal");
+		principal.focus();
+		return false;
+    }
+    var  r = document.getElementById("rate").value / 10;
+    if (r==0) {
+    	r = 10;
+    }
+    var y = document.getElementById("years").value;
+    if (y==0) { y = 1;}
+
+    var d = new Date();
+	var fut = Number(d.getFullYear());	
+	fut += Number(y)
+    rslt =  (p * y * r / 100);
+    ans = '<p>If you deposit <span class="yel">' + p  + '</span></p>'
+    ans += '<p>an interest rate of <span class="yel">' + r + '%. </span></p>';
+    ans = ans + '<p>You will receive an amount of <span class="yel">' + rslt + ',</span> </p>';
+    ans = ans + '<p>in the year <span class="yel">' + fut + '</span></p>'
+    document.getElementById("result").innerHTML = ans;
+    //alert(rslt)
+    return ans;
 }
         
